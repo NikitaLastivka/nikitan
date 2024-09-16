34c70b155c375cb5aa363a12f36ed56afb68026b
Никита <MrSuperskuy@gmail.com>
2024-09-16 21:11:03 +0300

    Add40

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,86 @@
 import * as THREE from '/node_modules/three/build/three.module.js';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from '/node_modules/three/examples/jsm/loaders/DRACOLoader.js';
+import { RGBELoader } from '/node_modules/three/examples/jsm/loaders/RGBELoader.js';
 
-let camera, scene, renderer, object, loader, mixer, model;
+let camera, scene, renderer, mixer, model, object;
 const clock = new THREE.Clock();
-
-const params = { asset: 'ballTake_sprite_reserve3' }; // Загружаемая модель по умолчанию
-const assets = ["Armature.001|Armature.001Action"]; // Массив анимаций
-
-window.onerror = function (message, source, lineno, colno, error) {
-    const errorMsg = `Error: ${message} at ${source}:${lineno}:${colno}`;
-    document.body.innerHTML += `<p style="color: red;">${errorMsg}</p>`;
-};
-
 init();
 
 function init() {
     const container = document.querySelector('.slot_machine');
 
-    // Камера
-    camera = new THREE.PerspectiveCamera(45, container.offsetWidth / container.offsetHeight, 1, 2000);
-    camera.position.set(50, 50, 180);
-
-    // Сцена
+    camera = new THREE.PerspectiveCamera( 45, container.offsetWidth / container.offsetHeight, 0.25, 300 );
+    //camera.position.set( - 1.8, 0.6, 2.7 );
+    camera.position.set(0, 27, 125 );
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xfb3c8c);
 
-    // Свет
-    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1.5);
-    hemiLight.position.set(0, 200, 0);
-    scene.add(hemiLight);
+    new RGBELoader()
+        .setPath( 'assets/js/textures/equirectangular/' )
+        .load( 'lauter_waterfall_1k.hdr', function ( texture ) {
+            texture.mapping = THREE.EquirectangularReflectionMapping;
 
-    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
-    dirLight.position.set(0, 74, 1000);
-    scene.add(dirLight);
+            scene.background = new THREE.Color(0xfb3c8c);
+            scene.environment = texture;
 
-    // Загрузка модели
-    loader = new GLTFLoader(); 
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/'); // Путь к декодерам Draco
-    dracoLoader.setDecoderConfig({ type: 'js' }); // Опционально: укажите тип (js или wasm)
+            render();
 
-    // Подключаем DRACOLoader к GLTFLoader
-    loader.setDRACOLoader(dracoLoader);
-    loadAsset(params.asset);
+            const loader = new GLTFLoader().setPath( 'assets/js/models/gltf/' );
+            loader.load( 'ballTake_sprite_reserve3.gltf', async function ( gltf ) {
 
-    // Рендерер
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(container.offsetWidth, container.offsetHeight);
+                model = gltf.scene;
+                object = gltf;
+                await renderer.compileAsync( model, camera, scene );
+
+                scene.add( model );
+
+                render();
+
+                if (gltf.animations && gltf.animations.length) {
+                    mixer = new THREE.AnimationMixer(model);
+                    const action = mixer.clipAction(gltf.animations[1]);
+                    action.play();
+                }
+
+                let loader = document.querySelector('.loader'),
+                    containerBlock = document.querySelector('.container');
+
+                containerBlock.style.display = 'flex';
+                onWindowResize();
+                loader.classList.remove('active');
+
+            } );
+
+        }
+     );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setAnimationLoop(animate);
-    renderer.shadowMap.enabled = true;
-    container.appendChild(renderer.domElement);
+    renderer.setSize( container.offsetWidth, container.offsetHeight );
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1;
+    container.appendChild( renderer.domElement );
 
-    // Управление
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 30, 0);
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set(0, 20, 0);
     controls.enableRotate = false;
     controls.enablePan = false;
     controls.enableZoom = false;
     controls.update();
 
-    window.addEventListener('resize', onWindowResize);
-
     let do_action2 = document.querySelector('.do_action');
     
     do_action2.onclick = function(){
         do_action2.onclick = false;
-        mixer = new THREE.AnimationMixer(object);
-        const action = mixer.clipAction(model.animations[0]);
+        mixer = new THREE.AnimationMixer(model);
+        const action = mixer.clipAction(object.animations[0]);
         action.loop = THREE.LoopOnce;
         action.clampWhenFinished = true;
         action.play();
-        console.log(action)
         setTimeout(()=>{
             let controlX = 0;
-            let controlY = 50;
+            let controlY = 20;
             let controlInterval = setInterval(()=>{
                     if(controlX < 5 || controlY > 10){
                         controlX += 0.2;
@@ -89,15 +94,15 @@ function init() {
             }, 10);
         }, 7000);
         setTimeout(()=>{
-            let camX = 50;
-            let camY = 50;
-            let camZ = 180;
+            let camX = 0;
+            let camY = 27;
+            let camZ = 125;
             let prize_container = document.querySelector('.prize_container');
             let CamInterval = setInterval(()=>{
                 if(camX > 17 || camY > 20 || camZ > 65){
                     camX > 17 ? camX -= 0.33 : 16.9;
-                    camY > 20 ? camY -= 0.3 : 19.9;
-                    camZ > 65 ? camZ -= 1.15 : 64.9; //44
+                    camY > 20 ? camY -= 0.07 : 19.9;
+                    camZ > 65 ? camZ -= 0.6 : 64.9; //44
                     camera.position.set(camX, camY, camZ);
                 }   
                 else{
@@ -110,49 +115,25 @@ function init() {
             }, 20);      
         }, 7900)
     }
-}
 
-function loadAsset(asset) {
-    loader.load(`https://smart-bike.nl/nikitan/assets/js/${asset}.glb`, (gltf) => {
-        if (object) scene.remove(object); // Удаляем предыдущий объект, если он существует
+    window.addEventListener( 'resize', onWindowResize );
 
-        object = gltf.scene; // Получаем сцену из загруженной модели
-        model = gltf;
-        object.traverse((child) => {
-            if (child.isMesh) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-            }
-        });
-        scene.add(object); // Добавляем объект в сцену
-
-        // Проверяем наличие анимаций и воспроизводим первую из них
-        if (gltf.animations && gltf.animations.length) {
-            mixer = new THREE.AnimationMixer(object); // Создаем анимационный микшер
-            const action = mixer.clipAction(gltf.animations[1]); // Получаем первую анимацию
-            action.play(); // Запускаем анимацию
-        }
-
-        let loader = document.querySelector('.loader'),
-            containerBlock = document.querySelector('.container');
-
-        containerBlock.style.display = 'flex';
-        onWindowResize();
-        loader.classList.remove('active');
-    }, 
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total * 100) + '% загружено');
-    },
-    (error) => {
-        console.error('Ошибка при загрузке GLTF файла:', error);
-    });
 }
 
 function onWindowResize() {
-    const container = document.querySelector('.slot_machine');
-    camera.aspect = container.offsetWidth / container.offsetHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
 }
 
 function animate() {
